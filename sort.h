b1c40794ad297d800dfdd2d9b4c6f66652468ab2
Tihitna-22 <tihitna222sisay@gmail.com>
2022-01-11 03:49:56 -0800

    modify for 1-insertion_sort_list

--- sort.h
+++ sort.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stddef.h>
 
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -21,9 +22,13 @@ typedef struct listint_s
 
 
 void print_array(const int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+
+
 #endif /* _SORT_H_ */
